1a51b5efe90b63407e487a4673b62a1f7329dcce
Aliza's <aliza.visuals@gmail.com>
2025-11-14 21:51:39 +0500

    Readme updated.

--- src/main/java/com/pluralsight/util/ReceiptWriter.java
+++ src/main/java/com/pluralsight/util/ReceiptWriter.java
@@ -17,24 +17,25 @@ public class ReceiptWriter {
         // makes sure to see if a receipts folder exists
         if (!folder.exists()) {
             folder.mkdir(); // create the folder if missing
+//        }
+
+            // creating a timestamp for the files
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+            String timeStamp = now.format(formatter);
+
+            // the file path for each new file that will be created based on new order
+            File completeOrderFile = new File("src/main/resources/receipts", timeStamp + ".txt");
+
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(completeOrderFile))) {
+
+                bufferedWriter.write(receiptText);
+                System.out.println("Receipt has been saved");
+            } catch (IOException e) {
+                System.out.println("Error saving receipt: " + e.getMessage());
+            }
         }
 
-        // creating a timestamp for the files
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
-        String timeStamp = now.format(formatter);
 
-        // the file path for each new file that will be created based on new order
-        File completeOrderFile = new File("src/main/resources/receipts", timeStamp + ".txt");
-
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(completeOrderFile))) {
-
-            bufferedWriter.write(receiptText);
-            System.out.println("Receipt has been saved");
-        } catch (IOException e) {
-            System.out.println("Error saving receipt: " + e.getMessage());
-        }
     }
-
-
 }
