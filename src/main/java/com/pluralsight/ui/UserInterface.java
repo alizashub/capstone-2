70a54884fd11b546b43efb3d972371827174bd05
Aliza's <aliza.visuals@gmail.com>
2025-11-14 07:58:35 +0500

    Made the output a little user friendly.

--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -1,11 +1,14 @@
 package com.pluralsight.ui;
-
 import com.pluralsight.models.*;
 import com.pluralsight.util.ReceiptWriter;
-
-import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.Scanner;
 
+/**
+ * UserInterface
+ * handles all console inputs/output
+ * controls the flow of placing the order
+ *
+ */
 public class UserInterface {
     private final Scanner myScanner;
     private Order currentOrder;
@@ -13,6 +16,7 @@ public class UserInterface {
     public UserInterface() {
         this.myScanner = new Scanner(System.in);
     }
+    // starts the main menu loop
 
     public void start() {
         boolean running = true;
@@ -25,7 +29,7 @@ public class UserInterface {
                     startNewOrder();
                     break;
                 case 0:
-                    System.out.println("Thank you for using Aliza's Deli App");
+                    System.out.println("Thank you for using Aliza's Deli App!");
                     running = false;
                     break;
                 default:
@@ -38,11 +42,14 @@ public class UserInterface {
 
     private int showHomeScreen() {
         System.out.println("""
-                Welcome!
-                1)New Order
-                0)Exit
-                Enter Your Choice:
-                """);
+                +++++++++++++++++++++++++++++
+                Welcome to Aliza's Deli App!!
+                +++++++++++++++++++++++++++++
+                
+                1) New Order
+                0) Exit
+                
+                Enter Your Choice: """);
         return readInt();
     }
 
@@ -71,7 +78,7 @@ public class UserInterface {
                     ordering = false;
                     break;
                 case 0:
-                    System.out.println("Order Cancelled");
+                    System.out.println("Order has been cancelled");
                     ordering = false;
                     break;
                 default:
@@ -82,20 +89,20 @@ public class UserInterface {
 
     private int showOrderScreen() {
         System.out.println("""
-                Order Menu:
+                ++++++++++++ Order Menu ++++++++++++
                 1) Add Sandwich
                 2) Add Drink
                 3) Add Chips
                 4) Add Sides
                 5) Checkout
                 0) Cancel Order
-                Enter your choice:
-                """);
+                Enter your choice: """);
         return readInt();
     }
 
     private void addSandwich() {
-        System.out.println("Let's Build A Sandwich");
+        System.out.println("\nLet's Build A Sandwich");
+
         SandwichSize size = chooseSandwichSize();
         BreadType breadType = chooseBreadType();
         boolean toasted = chooseToasted();
@@ -106,7 +113,7 @@ public class UserInterface {
         chooseSauces(sandwich);
 
         currentOrder.addItem(sandwich);
-        System.out.println("Sandwich has been added.");
+        System.out.println("Sandwich has been added!");
     }
 
     private SandwichSize chooseSandwichSize() {
@@ -138,6 +145,7 @@ public class UserInterface {
                 Would you like your bread toasted?
                 1) Yes
                 2) No
+                
                 Enter your choice : 
                 """);
         int choice = readIntInRange(1, 2);
@@ -145,25 +153,27 @@ public class UserInterface {
     }
 
     private void chooseToppings(Sandwich sandwich) {
-        System.out.println("Toppings Avalible");
+        System.out.println(" ++++++++++++ Toppings Avalible ++++++++++++");
         ToppingType[] tops = ToppingType.values();
 
         boolean adding = true;
 
         while (adding) {
-            System.out.println("Choose a Topping: ");
+            System.out.println("Choose a topping: ");
             for (int i = 0; i < tops.length; i++) {
                 System.out.println((i + 1) + ")" + tops[i].getDisplayName());
             }
             System.out.println("0) Done");
 
             int choice = readIntInRange(0, tops.length);
+
             if (choice == 0) break;
 
             ToppingType selected = tops[choice - 1];
 
             boolean extra = false;
 
+            // only meat & cheese has "extra" option
             if (selected.isMeat() || selected.isCheese()) {
                 System.out.println("""
                         Would you like to get an extra portion?
@@ -173,7 +183,7 @@ public class UserInterface {
                 extra = readIntInRange(1, 2) == 1;
             }
             sandwich.addTopping(new Topping(selected, extra));
-            System.out.println(selected.getDisplayName() + "added.");
+            System.out.println(selected.getDisplayName() + " added.");
         }
     }
 
@@ -183,7 +193,7 @@ public class UserInterface {
 
         boolean adding = true;
         while (adding) {
-            System.out.println("Choose a sauces you would like to add.");
+            System.out.println("Pick a sauces you would like to add.");
             for (int i = 0; i < sauces.length; i++) {
                 System.out.println((i + 1) + ")" + sauces[i].getDisplayName());
             }
@@ -191,19 +201,23 @@ public class UserInterface {
             int choice = readIntInRange(0, sauces.length);
             if (choice == 0) break;
 
+            SauceType selected = sauces [choice - 1];
+            sandwich.addSauce(selected);
+            System.out.println(selected.getDisplayName() + " added.");
+
         }
     }
 
     //DRINKS
 
     private void addDrink(){
-        System.out.println("Add Drinks");
+        System.out.println("\nAdd Drinks");
 
         DrinkSize size = chooseDrinkSize();
         DrinkFlavour flavour = chooseDrinkFlavour();
 
         currentOrder.addItem(new Drink("Drink",size,flavour));
-        System.out.println("Drink Added.");
+        System.out.println("Drink Added!\n");
     }
 
     private DrinkSize chooseDrinkSize() {
@@ -221,17 +235,17 @@ public class UserInterface {
     private DrinkFlavour chooseDrinkFlavour(){
         System.out.println("Choose Drink Flavour");
 
-        DrinkFlavour[] flavor = DrinkFlavour.values();
+        DrinkFlavour[] flavors = DrinkFlavour.values();
 
-        for (int i = 0; i < flavor.length; i++) {
-            System.out.println((i + 1) + ") " + flavor[i].getDisplayName());
+        for (int i = 0; i < flavors.length; i++) {
+            System.out.println((i + 1) + ") " + flavors[i].getDisplayName());
         }
-        int choice = readIntInRange(1,flavor.length);
-        return flavor[choice - 1];
+        int choice = readIntInRange(1,flavors.length);
+        return flavors[choice - 1];
     }
 
     private void addChips(){
-        System.out.println("Add Chips");
+        System.out.println("\nAdd Chips");
         ChipType[] chips = ChipType.values();
 
         for (int i = 0; i < chips.length; i++) {
@@ -241,11 +255,11 @@ public class UserInterface {
         int choice = readIntInRange(1, chips.length);
 
         currentOrder.addItem(new Chips("Chips", (chips [ choice - 1 ])));
-        System.out.println("Chips added!");
+        System.out.println("Chips added!\n");
     }
 
     private void addSide(){
-        System.out.println("Add Sides");
+        System.out.println("\nChoose Sides : ");
         SideType[] sides = SideType.values();
 
         for (int i = 0; i < sides.length; i++) {
@@ -254,17 +268,17 @@ public class UserInterface {
 
         int choice = readIntInRange(1, sides.length);
         currentOrder.addItem(new Side(sides[ choice - 1 ]));
-        System.out.println("Side added.");
+        System.out.println("Side added!\n");
     }
 
     private void checkout(){
-        System.out.println("Checkout");
+        System.out.println(" \n ++++++++ Checkout ++++++++ \n");
 
         String recieptText  = currentOrder.generateReceiptText();
-        System.out.println(recieptText );
+        System.out.println(recieptText);
 
-        ReceiptWriter.saveReceipt(recieptText );
-        System.out.println("Receipt saved!");
+        ReceiptWriter.saveReceipt(recieptText);
+        System.out.println("Receipt saved!\n");
     }
 
 
--- src/main/java/com/pluralsight/util/ReceiptWriter.java
+++ src/main/java/com/pluralsight/util/ReceiptWriter.java
@@ -6,9 +6,15 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * ReceiptWriter
+ * handles saving receipt text to a text file
+ * creates a unique timestamped file for each order
+ */
 public class ReceiptWriter {
     public static void saveReceipt(String receiptText) {
         File folder = new File("src/main/resources/receipts");
+        // makes sure to see if a receipts folder exists
         if (!folder.exists()) {
             folder.mkdir(); // create the folder if missing
         }
@@ -18,14 +24,15 @@ public class ReceiptWriter {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
         String timeStamp = now.format(formatter);
 
+        // the file path for each new file that will be created based on new order
         File completeOrderFile = new File("src/main/resources/receipts", timeStamp + ".txt");
 
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(completeOrderFile))) {
 
             bufferedWriter.write(receiptText);
-            System.out.println("Receipt saved");
+            System.out.println("Receipt has been saved");
         } catch (IOException e) {
-            System.out.println("Error" + e.getMessage());
+            System.out.println("Error saving receipt: " + e.getMessage());
         }
     }
 
